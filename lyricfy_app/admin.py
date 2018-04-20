1404dd0234c4c1c1ed97dc99c5421bf35c3959c4
marc <marcalba80@github.com>
2018-04-20 14:55:03 +0200

    new Models

--- lyricfy_app/admin.py
+++ lyricfy_app/admin.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
 import models
 
 admin.site.register(models.Song)
 admin.site.register(models.Author)
 admin.site.register(models.Playlist)
 admin.site.register(models.Lyrics)
+admin.site.register(models.Album)
 admin.site.register(models.Playlist_Song)
-
