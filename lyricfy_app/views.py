dd37a5e13b852c99cfcb35dd26186b4669d44797
adricasi <adriator6@gmail.com>
2018-05-08 13:01:31 +0200

    css and login redirect home

--- lyricfy_app/views.py
+++ lyricfy_app/views.py
@@ -4,10 +4,22 @@ from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
 from django.views.generic import CreateView
+
+from lyricfy_app.models import Song, Album
 
 
 # Create your views here.
+
+
+def Home(request):
+    template = "home.html"
+    context = {
+        "songs": Song.objects.all()[:10],
+        "albums": Album.objects.all()[:10],
+    }
+    return render(request, template, context)
 
 class Register(CreateView):
     model = User
