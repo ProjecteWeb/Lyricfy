5d39dc9506b482f09ffdea72fae5135b68d440e9
adricasi <adriator6@gmail.com>
2018-05-18 18:33:34 +0200

    l'usuari pot visualitzar les seves playlists

--- lyricfy_app/urls.py
+++ lyricfy_app/urls.py
@@ -6,4 +6,5 @@ from lyricfy_app import views
 urlpatterns = [
     url(r'^Inici/$', TemplateView.as_view(template_name="home.html"), name="home"),
     url(r'^Registre/$', views.Register.as_view(), name="registre"),
+    url(r'^Playlists/$', views.Playlists, name="playlists"),
 ]
--- lyricfy_app/views.py
+++ lyricfy_app/views.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
 from django.views.generic import CreateView
+
+from models import Playlist
 
 
 # Create your views here.
@@ -14,3 +17,11 @@ class Register(CreateView):
     template_name = "registration/registration.html"
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
+
+
+def Playlists(request):
+    template = 'Playlist/Playlists.html'
+    context = {
+        "playlists": Playlist.objects.filter(user=request.user)
+    }
+    return render(request, template, context)
