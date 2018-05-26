7ebb76d9b50afb63991263dedf90f2a8a500a333
ClaudiaMihaelaDiosan <mcd1591@gmail.com>
2018-05-26 22:09:01 +0200

    create playlist feature complete

--- features/steps/Create_Playlist.py
+++ features/steps/Create_Playlist.py
@@ -8,49 +8,15 @@ def step_impl(context):
     context.browser.visit(context.get_url('/lyricfy_app/Playlists/'))
 
 
-@step("I push the button Create playlist")
-def step_impl(context):
-    context.browser.find_by_tag('button').first.click()
-
-
-@then('I create a playlist "{MyPlaylist}" for "{user2}"')
+@step('I create a playlist "{MyPlaylist}" for "{user2}"')
 def step_impl(context, MyPlaylist, user2):
-    context.browser.fill("name", MyPlaylist)
-    from django.contrib.auth.models import User
-    from lyricfy_app.models import Playlist
-    playlist = Playlist()
-    playlist.user = User.objects.get(username=user2)
-    playlist.name = MyPlaylist
-    playlist.save()
-
-    context.browser.find_by_tag('button').first.click()
+    context.browser.find_by_name('create_playlist').first.click()
+    context.browser.find_by_id('id_name').fill(MyPlaylist)
+    context.browser.find_by_name('create_playlist').first.click()
 
     context.browser.visit(context.get_url('playlists'))
 
     assert context.browser.is_text_present(MyPlaylist)
-
-
-@step("I go to MyPlaylists and there are {count:n} playlist")
-def step_impl(context, count):
-    from lyricfy_app.models import Playlist
-    assert count == Playlist.objects.count()
-
-
-@then('I create a second playlist "{MyPlaylist1}" for "{user2}"')
-def step_impl(context, MyPlaylist1, user2):
-    context.browser.fill("name", MyPlaylist1)
-    from django.contrib.auth.models import User
-    from lyricfy_app.models import Playlist
-    playlist = Playlist()
-    playlist.user = User.objects.get(username=user2)
-    playlist.name = MyPlaylist1
-    playlist.save()
-
-    context.browser.find_by_tag('button').first.click()
-
-    context.browser.visit(context.get_url('playlists'))
-
-    assert context.browser.is_text_present(MyPlaylist1)
 
 
 @step("I go to MyPlaylists and there are {count:n} playlists")
@@ -59,19 +25,13 @@ def step_impl(context, count):
     assert count == Playlist.objects.count()
 
 
-@when('I create a playlist "{MyPlaylist}" for "{user2}"')
+@when('I create another playlist "{MyPlaylist}" for "{user2}"')
 def step_impl(context, MyPlaylist, user2):
-    context.browser.fill("name", MyPlaylist)
-    from django.contrib.auth.models import User
-    from lyricfy_app.models import Playlist
-    playlist = Playlist()
-    playlist.user = User.objects.get(username=user2)
-    playlist.name = MyPlaylist
-    playlist.save()
-
-    context.browser.find_by_tag('button').first.click()
+    context.browser.find_by_name('create_playlist').first.click()
+    context.browser.find_by_id('id_name').fill(MyPlaylist)
+    context.browser.find_by_name('create_playlist').first.click()
 
 
-@step("I get the Incorrect Creation Playlist error")
+@then("I get an error message")
 def step_impl(context):
-    assert context.browser.find_by_id('error')
+    assert context.browser.is_text_present('El nom de la playlist ja existeix')
